9d46ad3278f4fad1ff71625129d9dea6a97e8393
rajpatel923 <rajpatel123370@gmail.com>
2024-02-23 13:45:56 -0600

    created login button and signup button

--- app/src/main/java/com/example/snaproll/MainActivity.java
+++ app/src/main/java/com/example/snaproll/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.snaproll;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setting up the title
+        getSupportActionBar().setTitle("SnapRoll");
+
+        //open login
+        Button logInButton = findViewById(R.id.logInButton);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        //register activity
+        Button signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, signUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
