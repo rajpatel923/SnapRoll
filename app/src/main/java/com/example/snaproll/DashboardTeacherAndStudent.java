c8c535cea4ed4e14d16599f9e14f2be68de7ad13
rajpatel923 <rajpatel123370@gmail.com>
2024-02-28 15:15:56 -0600

    update to josh code

---
+++ app/src/main/java/com/example/snaproll/DashboardTeacherAndStudent.java
@@ -0,0 +1,54 @@
+package com.example.snaproll;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DashboardTeacherAndStudent extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_teacher_and_student);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.navRecent) {
+                    loadFragment(new RecentFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.navAttendance) {
+                    loadFragment(new AttendanceFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.navNotes) {
+                    loadFragment(new NotesFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.navProfile) {
+                    loadFragment(new ProfileFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+        loadFragment(new RecentFragment());
+    }
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
--- app/src/main/java/com/example/snaproll/MainActivity.java
+++ app/src/main/java/com/example/snaproll/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.snaproll;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,23 +17,24 @@ public class MainActivity extends AppCompatActivity {
        // getSupportActionBar().setTitle("SnapRoll");
 
         //open login
-//        Button logInButton = findViewById(R.id.logInButton);
-//        logInButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        Button logInButton = findViewById(R.id.logInButton);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DashboardTeacherAndStudent.class);
+                startActivity(intent);
+            }
+        });
 //        //register activity
-//        Button signUpButton = findViewById(R.id.signUpButton);
-//        signUpButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, signUpActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        Button signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, signUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
