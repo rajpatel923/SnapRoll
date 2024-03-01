0aa6521b1de62ecc66b6d0d6c58b1b5d7981d215
rajpatel923 <rajpatel123370@gmail.com>
2024-03-01 14:28:11 -0600

    Authentication with Email/Password

--- app/src/main/java/com/example/snaproll/signUpActivity.java
+++ app/src/main/java/com/example/snaproll/signUpActivity.java
@@ -1,14 +1,69 @@
 package com.example.snaproll;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class signUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class signUpActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    private EditText userName;
+   private EditText password;
+    private Button loginButton;
+    private TextView loginRedirectText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        auth = FirebaseAuth.getInstance();
+        userName = findViewById(R.id.signUpEmail);
+        password = findViewById(R.id.signUpPassword);
+        loginButton = findViewById(R.id.signUpButton);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = userName.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                if (user.isEmpty()){
+                    userName.setError("Email cannot be empty");
+                }
+                if(pass.isEmpty()){
+                    password.setError("Password cannot be empty");
+                }else{
+                    auth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(signUpActivity.this, "Signup Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(signUpActivity.this, MainActivity.class));
+                            }
+                            else{
+                                Toast.makeText(signUpActivity.this, "Signup Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(signUpActivity.this,LoginActivity.class));
+            }
+        });
     }
 }
